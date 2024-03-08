78192b8f579d352506102bdecbd514f9b99ddf99
Tobias <tobias@sernhede.com>
2024-03-08 13:46:08 +0100

    testing new way

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,17 +1,8 @@
-import { drizzle } from 'drizzle-orm/postgres-js'
-import postgres from 'postgres'
-import * as schema from '$lib/schema'
-import { DATABASE_URL } from '$env/static/private';
-
-// Disable prefetch as it is not supported for "Transaction" pool mode 
-
-
-
 import type { Handle } from '@sveltejs/kit';
-import { db } from '$lib/server/db';
+import { initiateDB } from '$lib/server/db';
 
 export const handle: Handle = async ({ event, resolve }) => {
-    event.locals.db = db;
+    event.locals.db = initiateDB();
     const response = await resolve(event);
     return response;
 }
--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -4,5 +4,8 @@ import * as schema from '$lib/schema'
 import { DATABASE_URL } from '$env/static/private';
 
 // Disable prefetch as it is not supported for "Transaction" pool mode 
-const client = postgres(DATABASE_URL, { prepare: false })
-export const db = drizzle(client, { schema });
+
+export const initiateDB = () => {
+    const client = postgres(DATABASE_URL, { prepare: false })
+    return drizzle(client, { schema });
+}

8b5df71a486c523ac954f6727e30784998f3af99
Tobias <tobias@sernhede.com>
2024-03-08 13:44:17 +0100

    testing other way

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -8,12 +8,10 @@ import { DATABASE_URL } from '$env/static/private';
 
 
 import type { Handle } from '@sveltejs/kit';
+import { db } from '$lib/server/db';
 
 export const handle: Handle = async ({ event, resolve }) => {
-
-    const client = postgres(DATABASE_URL, { prepare: false })
-    event.locals.db = drizzle(client, { schema });
-
+    event.locals.db = db;
     const response = await resolve(event);
     return response;
 }
